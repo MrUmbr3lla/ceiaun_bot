3e28f086085273609a9fc618f9ce8d877281264e
MrUmbr3lla <kianinima2000@gmail.com>
2024-01-29 21:58:18 +0330

    handling buttons

--- ceiaun_bot/main.py
+++ ceiaun_bot/main.py
@@ -12,106 +12,119 @@ from utils import create_new_sheet, write_data_to_sheet
 
 logger = logging.getLogger(__name__)
 
-keys = [[KeyboardButton("درخواست افزایش ظرفیت")],
-         [KeyboardButton("ابزار تبدیل متن") , KeyboardButton("چارت دروس")]]
-key_markup = ReplyKeyboardMarkup(keys , resize_keyboard=True , one_time_keyboard=True)
+pdf_file_ids = ["BQACAgQAAx0Edz5chAADA2W34cxmn7EW_DTt--kiUPL3WPmlAAIJEAACi4zBUU3kLcvAvafGNAQ" , #لیست نرم افزار
+                "BQACAgQAAx0Edz5chAADBGW34ileWM0Tg0cbAAF8xXR1ig7mSwACChAAAouMwVEy3dkOwcKm_DQE" , # لیست دروس فناوری اطلاعات
+                "BQACAgQAAx0Edz5chAADBWW34lzgzLL287tXKHGMZav4Omf_AAILEAACi4zBUaBr4xU4RUdINAQ" , # چارت پیشنهادی نرم افزار
+                "BQACAgQAAx0Edz5chAADBmW34pXyzuUw9KQUw9bCIUM4I-BjAAIMEAACi4zBUf6IfhfsqxqiNAQ"] # چارت پیشنهادی فناوری اطلاعات
 
-buttons = ["درخواست افزایش ظرفیت" , "چارت دروس" , "ابزار تبدیل متن"]
 
-### sending document and images
+#start commnad
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keys = [[KeyboardButton("درخواست افزایش ظرفیت")],
+            [KeyboardButton("ابزار تبدیل متن") , KeyboardButton("چارت دروس")]]
+    key_markup = ReplyKeyboardMarkup(keys , resize_keyboard=True , one_time_keyboard=True)
     await context.bot.send_document(
         chat_id=update.effective_chat.id,
-        document='BQACAgQAAxkBAAERRedltOR9eHOnJlCGmNmKlKQpkrVDHwAC4RAAAkpOqFHnytMdXl323zQE',
-        caption=messages.START_COMMAND,
+        document='BQACAgQAAx0Edz5chAADCWW340mbGOmLl-WAtkCOFxEm1L5kAAIPEAACi4zBUS36-Kc0q-JeNAQ',
         reply_markup=key_markup,
+        caption=messages.START_COMMAND,
         read_timeout= 20)
 
 
-async def courses_list_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pdf_file_ids = ["BQACAgQAAxkDAAPkZbZ_YkGd_DXFw_wyDBjH6EZRBmcAAnYUAAIZD7FRxvHkRVIWpJY0BA" , #لیست نرم افزار
-                     "BQACAgQAAxkDAAPrZbaBDjIFWbLpNHcCjo53B-FVK0cAAoUUAAIZD7FRA6rSpl77Sbc0BA" , # لیست دروس فناوری اطلاعات
-                       "BQACAgQAAxkDAAPtZbaBeZfNBWuqrG5Lhg5CpQdZOsYAAocUAAIZD7FRk3YrvhJRQoE0BA" , # چارت پیشنهادی نرم افزار
-                         "BQACAgQAAxkDAAPuZbaBsMeu0Rw1kCQG3F9f9RKYPBoAAokUAAIZD7FROhPmiPgnhh40BA"] # چارت پیشنهادی فناوری اطلاعات
-    for file_id in pdf_file_ids:
-        await context.bot.send_document(
-            chat_id=update.effective_chat.id,
-            document=file_id,
-            read_timeout= 20)
-
-
-async def request_sample_image_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_document(
-        chat_id=update.effective_chat.id,
-        document='BQACAgQAAxkDAAPQZbTmuFiI9B_rxHw_rSZGGRZEpygAAukQAAJKTqhRlM4x2o8RDP00BA',
-        caption=messages.REQUEST_COMMAND,
-        read_timeout= 20)
-    
-
-async def convert_name_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_document(
-        chat_id=update.effective_chat.id,
-        document='BQACAgQAAxkDAAPTZbTnHtO7g4RSzZpjpVermVDiNxUAAuoQAAJKTqhRx4DZ0PaIqU00BA',
-        caption=messages.CONVERT_NAME_COMMAND,
-        read_timeout= 20)
-###
-
-
+#دیگه فکر نکنم نیازی بهش باشه
 def handle_responses(text: str) -> str:
     processed: str = text
 
-    # convert course name for Amoozeshyar
-    if "تبدیل" in processed:
-        chars = list(processed)
-        course_name = chars[5:]
-        if course_name[-1] != " ":
-            course_name.append(" ")
-        modified_name = "".join([sub
-                                .replace(" ", "%")
-                                .replace("ی", "%")
-                                .replace("ک", "%")
-                                .replace("ي", "%")
-                                .replace("ك", "%")
-                                 for sub in course_name])
-        return modified_name
-
-    user_text = processed.split("+")
-
-    if len(user_text) != 4:
-        return messages.INCORRECT_LENGTH
-
-    student_name = user_text[0].strip()
-    student_id = str(unidecode(user_text[1].strip()))
-    student_course = user_text[2].strip()
-    student_course_id = str(unidecode(user_text[3].strip()))
-
-    if student_name.isnumeric():
-        return messages.INCORRECT_USERNAME
-
-    if (not student_id.isnumeric()) or (len(student_id) not in [8, 11, 14]):
-        return messages.INCORRECT_STUDENT_ID
-
-    if student_course.isnumeric():
-        return messages.INCORRECT_COURSE
-
-    if not student_course_id.isnumeric():
-        return messages.INCORRECT_COURSE_ID
-
-    # adding data to excel file
-    write_data_to_sheet(
-        settings.EXCEL_TEMP_FILE,
-        [student_name, student_id, student_course, student_course_id],
-        ["A", "B", "C", "D"],
-        start_row_number=3
-    )
-
-    return messages.RECEIVED_REQUEST
-
-
+###
 async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type = update.message.chat.type
     text = update.message.text
     users_id = update.message.from_user.username
+
+    #sending courses_list (done)
+    if "چارت دروس" in text:
+        
+         
+         for file_id in pdf_file_ids:
+            await context.bot.send_document(
+                chat_id=update.effective_chat.id,
+                document=file_id,
+                read_timeout= 20)
+
+
+
+    #converting courses name (unfinished)
+    if "ابزار تبدیل متن" in text:
+        await context.bot.send_document(
+        chat_id=update.effective_chat.id,
+        document="BQACAgQAAx0Edz5chAADCmW3440_ebd_3XPYwfxkoCpz8TYnAAIQEAACi4zBUfXtf4Z0LyMsNAQ",
+        caption=messages.CONVERT_NAME_COMMAND,
+        read_timeout= 20)
+
+
+        # باید با استیت هندل بشه ############################
+
+        # chars = list(text)
+        # course_name = chars[5:]
+        # if course_name[-1] != " ":
+        #     course_name.append(" ")
+        # modified_name = "".join([sub
+        #                         .replace(" ", "%")
+        #                         .replace("ی", "%")
+        #                         .replace("ک", "%")
+        #                         .replace("ي", "%")
+        #                         .replace("ك", "%")
+        #                          for sub in course_name])
+        # return modified_name
+
+
+
+    #students requests (unfinished)
+    if "درخواست افزایش ظرفیت" in text:
+        await context.bot.send_document(
+        chat_id=update.effective_chat.id,
+        document='BQACAgQAAx0Edz5chAADB2W34uUd86Q7EG8SNB8-jRtiPMZsAAINEAACi4zBURJXGMOkPaRENAQ',
+        caption=messages.REQUEST_COMMAND,
+        read_timeout= 20)
+
+
+    # باید با استیت هندل بشه ############################
+
+    # user_text = text.split("+")
+    # if len(user_text) != 4:
+    #     return messages.INCORRECT_LENGTH
+
+    # student_name = user_text[0].strip()
+    # student_id = str(unidecode(user_text[1].strip()))
+    # student_course = user_text[2].strip()
+    # student_course_id = str(unidecode(user_text[3].strip()))
+
+    # if student_name.isnumeric():
+    #     return messages.INCORRECT_USERNAME
+
+    # if (not student_id.isnumeric()) or (len(student_id) not in [8, 11, 14]):
+    #     return messages.INCORRECT_STUDENT_ID
+
+    # if student_course.isnumeric():
+    #     return messages.INCORRECT_COURSE
+
+    # if not student_course_id.isnumeric():
+    #     return messages.INCORRECT_COURSE_ID
+
+    # #adding data to excel file
+    # write_data_to_sheet(
+    #     settings.EXCEL_TEMP_FILE,
+    #     [student_name, student_id, student_course, student_course_id],
+    #     ["A", "B", "C", "D"],
+    #     start_row_number=3)
+
+    # return messages.RECEIVED_REQUEST
+###
+
+
+
+
+
 
     logger.info(f"user id is = {users_id}")
     logger.info(f" user{update.message.chat.id} in {message_type}: {text}")
@@ -134,10 +147,6 @@ if __name__ == "__main__":
     app.add_handlers([
         # Commands
         CommandHandler("start", start_command),
-        CommandHandler("chart_dorus" , courses_list_command),
-        CommandHandler("request" , request_sample_image_command),
-        CommandHandler("convert_name" , convert_name_command),
-
         # Messages
         MessageHandler(filters.TEXT, handle_messages),
     ])
