da867cd508e1ca7d6bd2ad91f21f7640eb59ad2c
Mobin Ghoveoud <mobin.sgh@gmail.com>
2024-01-30 03:33:17 +0330

    Handle request course

--- ceiaun_bot/main.py
+++ ceiaun_bot/main.py
@@ -7,7 +7,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes, Conversation
 
 import settings
 from messages import keyboards, messages
-from utils import create_new_sheet
+from utils import create_new_sheet, process_course_request
 
 logger = logging.getLogger(__name__)
 
@@ -90,7 +90,7 @@ async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if text == keyboards.HOME_CONVERT_NAME:
         await context.bot.send_document(
             chat_id=update.effective_chat.id,
-            document="BQACAgQAAx0Edz5chAADCmW3440_ebd_3XPYwfxkoCpz8TYnAAIQEAACi4zBUfXtf4Z0LyMsNAQ",
+            document=settings.FILE_CONVERT_NAME,
             caption=messages.CONVERT_NAME_COMMAND,
             reply_markup=keyboards.BACK_KEYBOARD,
         )
@@ -98,45 +98,15 @@ async def handle_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return CONVERT_COURSE
 
     # students requests (unfinished)
-    if "درخواست افزایش ظرفیت" in text:
+    if text == keyboards.HOME_COURSE_REQUEST:
         await context.bot.send_document(
             chat_id=update.effective_chat.id,
-            document='BQACAgQAAx0Edz5chAADB2W34uUd86Q7EG8SNB8-jRtiPMZsAAINEAACi4zBURJXGMOkPaRENAQ',
+            document=settings.FILE_COURSE_REQUEST,
             caption=messages.REQUEST_COMMAND,
+            reply_markup=keyboards.BACK_KEYBOARD
         )
 
-    # باید با استیت هندل بشه ############################
-
-    # user_text = text.split("+")
-    # if len(user_text) != 4:
-    #     return messages.INCORRECT_LENGTH
-
-    # student_name = user_text[0].strip()
-    # student_id = str(unidecode(user_text[1].strip()))
-    # student_course = user_text[2].strip()
-    # student_course_id = str(unidecode(user_text[3].strip()))
-
-    # if student_name.isnumeric():
-    #     return messages.INCORRECT_USERNAME
-
-    # if (not student_id.isnumeric()) or (len(student_id) not in [8, 11, 14]):
-    #     return messages.INCORRECT_STUDENT_ID
-
-    # if student_course.isnumeric():
-    #     return messages.INCORRECT_COURSE
-
-    # if not student_course_id.isnumeric():
-    #     return messages.INCORRECT_COURSE_ID
-
-    # #adding data to excel file
-    # write_data_to_sheet(
-    #     settings.EXCEL_TEMP_FILE,
-    #     [student_name, student_id, student_course, student_course_id],
-    #     ["A", "B", "C", "D"],
-    #     start_row_number=3)
-
-    # return messages.RECEIVED_REQUEST
-    ###
+        return REQUEST_COURSE
 
     logger.info(f"user id is = {users_id}")
     logger.info(f" user{update.message.chat.id} in {message_type}: {text}")
@@ -169,6 +139,17 @@ async def handle_convert_course(update: Update, context: ContextTypes.DEFAULT_TY
     )
 
 
+async def handle_request_course(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    result = process_course_request(text)
+
+    await update.message.reply_text(
+        text=result,
+        reply_markup=keyboards.BACK_KEYBOARD,
+        quote=True
+    )
+
+
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.warning(f"Error for update {update} caused error {context.error}")
 
@@ -192,8 +173,12 @@ if __name__ == "__main__":
             )
         ],
         states={
-            HOME: [MessageHandler(filters.TEXT, handle_messages)],
-            # REQUEST_COURSE: [MessageHandler(filters.TEXT, handle_request_course)],
+            HOME: [
+                MessageHandler(filters.TEXT, handle_messages)
+            ],
+            REQUEST_COURSE: [
+                MessageHandler(filters.TEXT & ~filters.Regex(f"^{keyboards.BACK}$"), handle_request_course)
+            ],
             CONVERT_COURSE: [
                 MessageHandler(filters.TEXT & ~filters.Regex(f"^{keyboards.BACK}$"), handle_convert_course)
             ],
--- ceiaun_bot/utils/__init__.py
+++ ceiaun_bot/utils/__init__.py
@@ -1,5 +1,7 @@
 from unidecode import unidecode
 
+import settings
+from messages import messages
 from .excel import create_new_sheet, write_data_to_sheet  # noqa
 
 
@@ -28,3 +30,36 @@ def clean_data(data: str, has_number: list = None) -> list[str]:
         cleaned_data.append(value.strip())
 
     return cleaned_data
+
+
+def process_course_request(text: str) -> str:
+    user_text = text.split("+")
+    if len(user_text) != 4:
+        return messages.REQUEST_INCORRECT_LENGTH
+
+    student_name = user_text[0].strip()
+    student_id = str(unidecode(user_text[1].strip()))
+    student_course = user_text[2].strip()
+    student_course_id = str(unidecode(user_text[3].strip()))
+
+    if student_name.isnumeric():
+        return messages.REQUEST_INCORRECT_USERNAME
+
+    if (not student_id.isnumeric()) or (len(student_id) not in [8, 11, 14]):
+        return messages.REQUEST_INCORRECT_STUDENT_ID
+
+    if student_course.isnumeric():
+        return messages.REQUEST_INCORRECT_COURSE
+
+    if not student_course_id.isnumeric():
+        return messages.REQUEST_INCORRECT_COURSE_ID
+
+    # adding data to excel file
+    write_data_to_sheet(
+        settings.EXCEL_TEMP_FILE,
+        [student_name, student_id, student_course, student_course_id],
+        ["A", "B", "C", "D"],
+        start_row_number=3
+    )
+
+    return messages.REQUEST_RECEIVED_REQUEST
